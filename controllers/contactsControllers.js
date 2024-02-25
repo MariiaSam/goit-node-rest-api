61a7319078e96c74445013ca61b5863b2e9232ab
MariiaSam <kontragent@ukr.net>
2024-02-25 21:36:23 +0200

    add

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,56 @@
-import contactsService from "../services/contactsServices.js";
+import { Contact } from "../models/contact.js";
+import wrapper from "../helpers/wrapper.js";
+import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = wrapper(async (req, res) => {
+  const allContacts = await Contact.find();
+  res.json(allContacts);
+});
 
-export const getOneContact = (req, res) => {};
+export const getOneContact =  wrapper (async (req, res) => {
+  const { id } = req.params;
+  const contactById = await Contact.findById(id);
+  if (!contactById) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(contactById);
+});
 
-export const deleteContact = (req, res) => {};
+export const createContact = wrapper (async (req, res) => {
+  const newContact = await Contact.create(req.body);
+  res.status(201).json(newContact);
+});
 
-export const createContact = (req, res) => {};
+export const updateContact = wrapper (async (req, res) => {
+  const { id } = req.params;
+  const resultUpdate = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
 
-export const updateContact = (req, res) => {};
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.json(resultUpdate);
+});
+
+export const updateFavorite = wrapper(async (req, res) => {
+  const { id } = req.params;
+  const resultFavorite = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+
+  if (!resultFavorite) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(resultFavorite);
+});
+
+export const deleteContact = wrapper (async (req, res) => {
+  const { id } = req.params;
+  const resultDelete = await Contact.findByIdAndDelete(id);
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(resultDelete);
+});
---
+++ models/contact.js
@@ -0,0 +1,52 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+import { middlewares } from "../middleware/index.js";
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", middlewares.mongooseError);
+
+export const Contact = model("contact", contactSchema);
+
+ const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required().messages({ "any.required": "Missing required name field" }),
+  email: Joi.string().email().required().messages({ "any.required": "Missing required name field" }),
+  phone: Joi.string().required().messages({ "any.required": "Missing required name field" }),
+  favorite: Joi.boolean(),
+});
+
+ const updateContactSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+});
+
+ const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required().messages({
+    "any.required": "Missing required favorite field",
+  }),
+});
+
+export const schemas = {
+    createContactSchema,
+    updateContactSchema,
+    updateFavoriteSchema
+}
